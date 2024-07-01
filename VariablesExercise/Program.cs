52821b7f6173f0a5c55c7e37aa63461f68d2bb51
Stephen-May1_Dell <stephen_may1@dell.com>
2024-07-01 11:27:55 -0400

    declare variables and instantiate

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Bob";
+            int age = 40;
+            char exclamation = '!';
+            bool isProgrammer = true;
+            double wage = 20.00;
+            decimal salary = 50000.00m;
+
+            Console.WriteLine($"{name} is {age} years old and it is {isProgrammer.ToString()} that he is a programmer{exclamation} He makes ${wage} an hour for a salary of {salary}");
         }
     }
 }
